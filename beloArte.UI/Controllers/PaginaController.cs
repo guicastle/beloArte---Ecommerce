37a623a7822839684c1b09ef71d448f5238f400f
guicastle <guilhermecastelobrancomuniz@hotmail.com>
2017-09-20 00:30:20 -0300

    chg: Refatorações na pagina principal

---
+++ beloArte.UI/Controllers/PaginaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace beloArte.UI.Controllers
+{
+    public class PaginaController : Controller
+    {
+        // GET: Home
+        public ActionResult Principal()
+        {
+            return View("Index");
+        }
+    }
+}
