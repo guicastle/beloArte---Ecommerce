693a9d16e89a0437b26dbbe32fc289978d8dbbd4
guicastle <guilhermecastelobrancomuniz@hotmail.com>
2017-09-20 23:37:50 -0300

    new: Nova tela de lista de produtos

---
+++ beloArte.UI/Controllers/Produto/ProdutoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace beloArte.UI.Controllers.Produto
+{
+    public class ProdutoController : Controller
+    {
+        // GET: Produto
+        public ActionResult Catalago()
+        {
+            return View("Produto");
+        }
+    }
+}
