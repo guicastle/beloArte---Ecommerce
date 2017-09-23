4855b45a01bea48bc1883df2ed98b2a4d05d908c
guicastle <guilhermecastelobrancomuniz@hotmail.com>
2017-09-22 21:10:33 -0300

    new: Novas classes Carrinho e Feedback

---
+++ beloArte.UI/Controllers/Carrinho/CarrinhoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace beloArte.UI.Controllers.Carrinho
+{
+    public class CarrinhoController : Controller
+    {
+        // GET: Carrinho
+        public ActionResult ProdutosAdd()
+        {
+            return View();
+        }
+    }
+}
---
+++ beloArte.UI/Controllers/Feedback/FeedbackController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace beloArte.UI.Controllers.Feedback
+{
+    public class FeedbackController : Controller
+    {
+        // GET: Feedback
+        public ActionResult EnviarOpniao()
+        {
+            return View();
+        }
+    }
+}
